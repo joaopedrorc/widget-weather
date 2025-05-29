d58713f60d42b8b92bf0298405ab4e8dd748605e
João Pedro Cordeiro <jpc5626@gmail.com>
2025-05-29 10:32:43 -0300

    feat: connection mf

--- module-federation.config.ts
+++ module-federation.config.ts
@@ -1,5 +1,12 @@
 export const mfConfig = {
   name: "widget_weather",
-  exposes: {},
+  filename: "remoteEntry.js",
+  exposes: {
+    "./App": "./src/App", // Adjust path as needed
+  },
   shared: ["react", "react-dom"],
+  library: {
+    type: "var",
+    name: "widget_weather", // This will define `window.widget_weather`
+  },
 };
